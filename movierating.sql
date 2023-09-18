837592d3aed81fdf6367edcc3f80abd2ace32417
MRobinson112 <139254094+MRobinson112@users.noreply.github.com>
2023-09-17 21:30:57 -0400

    Add files via upload

---
+++ movierating.sql
@@ -0,0 +1,55 @@
+create database MovieRating;
+create table movie_ratings (
+movie varchar(50),
+rater varchar(50),
+rating int
+ );
+ INSERT INTO movie_ratings (movie, rater, rating)
+VALUES
+    ('Equalizer 3', 'Jerrell', 5),
+    ('Equalizer 3', 'Mikhail', 5),
+    ('Equalizer 3', 'Nickolei', 5),
+    ('Equalizer 3', 'Garvey', 4),
+	('Equalizer 3', 'Ernest', 3);
+ INSERT INTO movie_ratings (movie, rater, rating)
+VALUES
+    ('The Flash', 'Jerrell', 4),
+    ('The Flash', 'Mikhail', 2),
+    ('The Flash', 'Nickolei', 2),
+    ('The Flash', 'Garvey', null),
+	('The Flash', 'Ernest', 3);
+ INSERT INTO movie_ratings (movie, rater, rating)
+VALUES
+    ('Blue Beetle', 'Jerrell', 5),
+    ('Blue Beetle', 'Mikhail', 4),
+    ('Blue Beetle', 'Nickolei', 3),
+    ('Blue Beetle', 'Garvey', 4),
+	('Blue Beetle', 'Ernest', 2);
+ INSERT INTO movie_ratings (movie, rater, rating)
+VALUES
+    ('Spiderman', 'Jerrell', 4),
+    ('Spiderman', 'Mikhail', 4),
+    ('Spiderman', 'Nickolei', 4),
+    ('Spiderman', 'Garvey', 5),
+	('Spiderman', 'Ernest', 4);
+ INSERT INTO movie_ratings (movie, rater, rating)
+VALUES
+    ('Barbie', 'Jerrell', 1),
+    ('Barbie', 'Mikhail', 2),
+    ('Barbie', 'Nickolei', 1),
+    ('Barbie', 'Garvey', 1),
+	('Barbie', 'Ernest', 4);
+ INSERT INTO movie_ratings (movie, rater, rating)
+VALUES
+    ('Gran Turismo', 'Jerrell', 5),
+    ('Gran Turismo', 'Mikhail', 5),
+    ('Gran Turismo', 'Nickolei', null),
+    ('Gran Turismo', 'Garvey', 5),
+	('Gran Turismo', 'Ernest', null);
+
+
+
+
+
+
+ 
